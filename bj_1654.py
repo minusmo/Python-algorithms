ac8e4af8152d9439c1e070e2733beb562cc39be2
hojooneum <bldolphin96@gmail.com>
2021-11-16 23:56:44 +0900

    bj_1654 not solved yet

--- bj_1654.py
+++ bj_1654.py
@@ -4,13 +4,40 @@ lines = []
 for _ in range(k):
     line = int(input())
     lines.append(line)
-minimum = min(lines)
-if n == k:
-    print(max(lines))
-else:
-    if max(lines) 
-    for i in range(minimum, 0, -1):
-        lans = [j//i for j in lines]
-        if sum(lans) >= n:
-            print(i)
-            break
+# minimum = min(lines)
+
+# max_len = -1
+# for i in range(minimum, 0, -1):
+#     lans = [j//i for j in lines]
+#     if sum(lans) >= n:
+#         max_len = i
+#         break
+M = max(lines)
+m = -1
+length = -1
+lengs = [i for i in range(1,M+1)]
+start = 0
+end = M+1
+while start < end:
+    lengs = lengs[start:end]
+    mid = lengs[round((start+end)/2)]
+    if sum([line//mid for line in lines]) >= n and mid > m:
+        m = mid
+        start = round((start+end)/2)
+    else:
+        end = round((start+end)/2)
+print(m)
+
+# 4 4
+# 10
+# 3
+# 1
+# 1
+# :3
+
+# 4 5
+# 10 
+# 1
+# 1
+# 1
+# :2
