0a7b2137caa0c551d872a42e6d24c3b2bde837b0
hojooneum <bldolphin96@gmail.com>
2021-12-01 16:05:48 +0900

    bj_2579 solved

--- bj_2579.py
+++ bj_2579.py
@@ -11,12 +11,26 @@ def max_score(stairs):
         return stairs[0]
     elif len(stairs) == 2:
         return stairs[0]+stairs[1]
+    elif len(stairs) == 3:
+        return max(stairs[0]+stairs[2], stairs[1]+stairs[2])
     
     stairs = [0] + stairs
     max_stairs[1] = stairs[1]
     max_stairs[2] = stairs[1]+stairs[2]
-    for i in range(3, len(stairs)+1):
-        max_stairs[i] = stairs[i]+max(max_stairs[i-1] if max_stairs[i-1] != max_stairs[i-2]+stairs[i-1] else -1, max_stairs[i-2])
+    for i in range(3, len(stairs)):
+        max_stairs[i] = stairs[i]+max(stairs[i-1]+max_stairs[i-3], max_stairs[i-2])
     return max_stairs[-1]
 
 print(max_score(stairs))
+# 케이스를 정확히 분리할것!!!
+# 10
+# 100
+# 100
+# 1
+# 1
+# 100
+# 100
+# 1
+# 1000
+# 1000
+# 1000
