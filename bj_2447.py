c8d08cb44e093ea4763fe5d1c66b3cc15f8ea1bd
hojooneum <bldolphin96@gmail.com>
2021-12-07 23:45:55 +0900

    working on bj_2447

---
+++ bj_2447.py
@@ -0,0 +1,15 @@
+N = int(input())
+
+def three_star(n):
+    line1 = []
+    line2 = []
+    line3 = []
+    # if n == 3:
+    #     return '*'*3
+    line1.append(three_star(n/3)*3)
+    for i in range(3):
+        pass
+    for _ in range(3):
+        pass
+    
+    return [line1, line2, line3]
