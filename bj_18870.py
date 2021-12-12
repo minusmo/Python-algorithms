1001518e73492b055a09a3fc81fa3ba6f53ce83d
hojooneum <bldolphin96@gmail.com>
2021-12-12 23:36:15 +0900

    still working on bj_18870

--- bj_18870.py
+++ bj_18870.py
@@ -3,10 +3,10 @@ xs = list(map(int, input().split()))
 
 def compress_coord(xs):
     x_compressed = []
-    for ind, x in enumerate(xs):
-        xp = [i-x for i in xs]
-        xp = sum([1 if i<0 else 0 for i in xp])
-        x_compressed.append(xp)
+    xs = sorted(xs)
+    
+    
+    
     return x_compressed
 
 for xp in compress_coord(xs):
