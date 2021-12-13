e4f361f9014d6d63bd8d79277b2611f7c01a8ffb
hojooneum <bldolphin96@gmail.com>
2021-12-13 23:05:07 +0900

    bj_18870 solved

--- bj_18870.py
+++ bj_18870.py
@@ -3,10 +3,11 @@ xs = list(map(int, input().split()))
 
 def compress_coord(xs):
     x_compressed = []
-    xs = sorted(xs)
-    
-    
-    
+    xss = list(sorted(set(xs)))
+    inds = [i for i in range(len(xss))]
+    d = dict(zip(xss, inds))
+    for x in xs:
+        x_compressed.append(d[x])
     return x_compressed
 
 for xp in compress_coord(xs):
