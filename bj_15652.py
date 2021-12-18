c92509880d8fda91c441249e4b11121290b464e0
hojooneum <bldolphin96@gmail.com>
2021-12-18 22:37:42 +0900

    bj 15652 solved

---
+++ bj_15652.py
@@ -0,0 +1,39 @@
+# back tracking
+
+N, M = map(int, input().split())
+
+def unique_series(n,m):
+    def isAvailable(series,i):
+        # promising
+        if len(series) == 0:
+            return True
+        if i >= series[-1]:
+            return True
+        return False
+    
+    def dfs(n,m,series):
+        if m == 0:
+            print(" ".join(map(str,series)))
+            return
+        
+        for i in range(1, n+1):
+            if isAvailable(series,i):
+                series.append(i)
+                dfs(n,m-1,series)
+                # pruning
+                series.pop()
+    
+    dfs(n,m,[])
+    
+
+unique_series(N,M)
+
+"""
+dfs를 사용할 때는
+state space tree(상태 공간 트리):
+문제 해결 과정의 중간 상태를 각각의 노드로 나타낸 트리
+를 생각하자
+
+탐색의 범위와 차례도 생각할것!
+트리의 형태로 탐색을 진행 한다!
+"""
