6224fde58f911c0d8f560c8af82ebf3901e3cdcc
hojooneum <bldolphin96@gmail.com>
2021-12-21 23:33:13 +0900

    still working on bj 9663

--- bj_9663.py
+++ bj_9663.py
@@ -1,31 +1,41 @@
 # 백트래킹 예제: n by n 체스판에 n개의 퀸을 놓는 경우의 수를 출력하는 문제
 
-def nqueen(n):
+def isAvailable(currentCandidate, currentCol):
+    currentRow = len(currentCandidate)
+    for i in range(currentRow):
+        if currentCandidate[i] == currentCol or (abs(currentCandidate[i]-currentCol)) == currentRow-i:
+            return False
+    return True
     
-    def isAvailable(currentCandidate, currentCol):
-        currentRow = len(currentCandidate)
-        # promising: 가능한 경우의 수를 체크하는 과정
-        for i in range(currentRow):
-            # promising 중 같은 column에 있는지 혹은 대각선에 있는지를 검사
-            if currentCandidate[i] == currentCol or (abs(currentCandidate[i]-currentCol)) == currentRow-i:
-                return False
-        return True
+def dfs(n, currentRow, currentCandidate):
+    ways = 0
+    if currentRow == n:
+        return 1
     
-    def dfs(n, currentRow, currentCandidate):
-        if currentRow == n:
-            # print(currentCandidate)
-            return 
-        
-        for i in range(n):
-            if isAvailable(currentCandidate, i):
-                currentCandidate.append(i)
-                dfs(n, currentRow+1, currentCandidate)
-  
-                # pruning: 가능하지 않은 경우의 수를 제거하는 과정
-                currentCandidate.pop(len(currentCandidate)-1)
+    for i in range(n):
+        if isAvailable(currentCandidate, i):
+            currentCandidate.append(i)
+            cands = dfs(n, currentRow+1, currentCandidate)
+            ways += cands
+            currentCandidate.pop(len(currentCandidate)-1)
+    return ways
 
-    return dfs(n, 0, [])
+def nqueen(n):
+    nq = [0] * (n+1)
+    if n == 1:
+        return 1
+    if n < 5:
+        return 0
+    if n == 5:
+        return 10
+    nq[1] = 1
+    nq[5] = 10
+    
+    for i in range(6, n+1):
+        nq[i] = nq[i-1] + dfs(i,1,[i-1])
+    return nq[n]
     
 n = int(input())
 
 print(nqueen(n))
+# print(len(nqueen(n)))
