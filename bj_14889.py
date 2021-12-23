8fbaa90daa471621b06ef2e6b14796889c57bc74
hojooneum <bldolphin96@gmail.com>
2021-12-24 01:01:00 +0900

    keep optimizing bj 14889

--- bj_14889.py
+++ bj_14889.py
@@ -11,8 +11,8 @@ chosen = [0]*(N+1)
 
 def calculate(team):
     total_stat = 0
-    for i in team:
-        for j in team:
+    for i in team[1:]:
+        for j in team[1:]:
             if i != j:
                 total_stat += stats[i-1][j-1]
     return total_stat
@@ -26,7 +26,10 @@ def search(n):
                 link.append(i)
         diff = abs(calculate(start) - calculate(link))
         min_diff = min(min_diff, diff)
+        link = [0]
     else:
+        if chosen[1] == 0 and sum(chosen[1:]) == 1:
+            return
         for i in range(1, N+1):
             if chosen[i] == 0:
                 start.append(i)
@@ -38,12 +41,38 @@ def search(n):
 search(0)
 print(min_diff)
 """
-123
+1234
 
-12
-13
-21
-23
-31
-32
+12 34
+13 24
+14 23
+---
+21 x
+23 x
+24 x
+31 x
+32 x
+34 x
+41 x
+42 x
+43 x
+
+123456
+
+123 456
+124 356
+125 346
+126 345
+134 256
+135 246
+136 245
+145 236
+146 235
+156 234
+---
+213
+214
+215
+216
+...
 """
