ee89a035f3c94651731d74607c17a1b851aaed1d
hojooneum <bldolphin96@gmail.com>
2021-12-25 23:45:17 +0900

    still optimizing bj 14889

--- bj_14889.py
+++ bj_14889.py
@@ -16,15 +16,32 @@ def calculate(team):
             if i != j:
                 total_stat += stats[i-1][j-1]
     return total_stat
-                
-def search(n):
+
+def find_pairs(start):
+    last_added = start[-1]
+    pairs_left = [(i, last_added) for i in start[:-1]]
+    pairs_right = [(last_added, i) for i in start[:-1]]
+    return pairs_left + pairs_right
+
+def calculate_pairs(pairs):
+    total_stat = 0
+    for pair in pairs:
+        total_stat += stats[pair[0]-1, pair[1]-1]
+    return total_stat
+
+def search(n, calculated_stat):
     global min_diff, start, link, chosen
+    if n >= 2:
+        pairs = find_pairs(start)
+        total_stat = calculate_pairs(pairs)
+        calculated_stat += total_stat
     if n == N/2:
         # calculate difference
         for i in range(1, N+1):
             if chosen[i] == 0:
                 link.append(i)
-        diff = abs(calculate(start) - calculate(link))
+        
+        diff = abs(calculated_stat - calculate(link))
         min_diff = min(min_diff, diff)
         link = [0]
     else:
@@ -34,11 +51,11 @@ def search(n):
             if chosen[i] == 0:
                 start.append(i)
                 chosen[i] = 1
-                search(n+1)
+                search(n+1, calculated_stat)
                 start.pop()
                 chosen[i] = 0
 
-search(0)
+search(0, 0)
 print(min_diff)
 """
 1234
@@ -59,11 +76,11 @@ print(min_diff)
 
 123456
 
-123 456
+123 456 / 12 21 / 13 23 31 32 / 17 27 37 71 72 73 
 124 356
 125 346
 126 345
-134 256
+134 256 / 13 31 / 14 34 41 43
 135 246
 136 245
 145 236
