62ed6655ceac346dfc8b7a81e3116486a90ed5ca
hojooneum <bldolphin96@gmail.com>
2022-01-23 23:52:35 +0900

    working on bj_1904 timeout error

--- bj_1904.py
+++ bj_1904.py
@@ -10,13 +10,11 @@ def number_of_binary_series_with_00and1(n):
         number_of_binary_series[1] = 1
         number_of_binary_series[2] = 2
         for i in range(3,n+1):
-            if i%2 == 0:
-                pass
-            else:
-                number_of_binary_series[i] = (number_of_binary_series[i-1])*2 - 1
-                
-                
-                
+            number_of_binary_series[i] = (2 * number_of_binary_series[i-1] + 2 * number_of_binary_series[i-2]) // 2
+        return number_of_binary_series[n]
+
+print(number_of_binary_series_with_00and1(N))
+
 """
 111
 
@@ -26,4 +24,10 @@ def number_of_binary_series_with_00and1(n):
 00
 11
 
+n 번째:
+
+n-1 + 1 : 오른쪽에 1
+1 + n-1: 왼쪽에 1
+n-2 + 00 : 오른쪽에 00
+00 + n-2 : 왼족에 00 
 """
