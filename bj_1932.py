9e43be75c76ef77cbb50505ff7338a781c5c3e56
hojooneum <bldolphin96@gmail.com>
2022-01-27 01:40:28 +0900

    working on bj_1932...

---
+++ bj_1932.py
@@ -0,0 +1,22 @@
+def intTriangle():
+    N = int(input())
+    numbers_in_triangle = [[-1]]
+    max_sum_at_each_number = [[-1]]
+    for _ in range(N):
+        numbers_in_each_level = get_numbers_in_each_level()
+        numbers_in_triangle.append(numbers_in_each_level)
+        max_sum_at_each_number.append([0]*len(numbers_in_each_level))
+    
+    for row in range(1, N+1):
+        for i in range(row):
+            try:
+                max_sum_at_each_number[row][i] = bigger_sum_in_two_route(max_sum_at_each_number[row-1][i], max_sum_at_each_number[row-1][i+1]) + numbers_in_triangle[row][i]
+            except:
+                max_sum_at_each_number[row][i] = max_sum_at_each_number[row][i]
+    return max(max_sum_at_each_number[N])
+
+def get_numbers_in_each_level():
+    return list(map(int, input().split()))
+
+def bigger_sum_in_two_route(parent_left, parent_right):
+    
