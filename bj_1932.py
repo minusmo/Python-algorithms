6803fd69e4ab6543e613ba60c77903a1523f306c
hojooneum <bldolphin96@gmail.com>
2022-01-27 17:31:01 +0900

    venv created for unittest

--- bj_1932.py
+++ bj_1932.py
@@ -9,14 +9,16 @@ def intTriangle():
     
     for row in range(1, N+1):
         for i in range(row):
-            try:
+            if has_no_parent_right(row, i):
+                max_sum_at_each_number[row][i] = max_sum_at_each_number[row-1][i] + numbers_in_triangle[row][i]
+            elif has_no_parent_both(row, i):
+                max_sum_at_each_number[row][i] = 
+            else:
                 max_sum_at_each_number[row][i] = bigger_sum_in_two_route(max_sum_at_each_number[row-1][i], max_sum_at_each_number[row-1][i+1]) + numbers_in_triangle[row][i]
-            except:
-                max_sum_at_each_number[row][i] = max_sum_at_each_number[row][i]
     return max(max_sum_at_each_number[N])
 
 def get_numbers_in_each_level():
     return list(map(int, input().split()))
 
 def bigger_sum_in_two_route(parent_left, parent_right):
-    
+    return max(parent_left, parent_right)
