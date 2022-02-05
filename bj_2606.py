6f185316f599a812b39c5fdb0c19c7f0378b01cf
hojooneum <bldolphin96@gmail.com>
2022-02-05 22:43:59 +0900

    bj_2606 solved!

--- bj_2606.py
+++ bj_2606.py
@@ -1,17 +1,33 @@
 class WormVirus:
-    def __init__(self, computers, pairs) -> None:
-        self.computers = self.computers
+    def __init__(self, computers, pairs):
+        self.computers = computers
         self.pairs = pairs
         self.computerGraph = [[] for _ in range(self.computers + 1)]
         self.linkComputers()
-        self.infectedComputers = [0] * (self.computers + 1)
-        self.needVisit = [1]
+        self.infectedComputers = [0] * (self.computers + 1) #큐
+        self.needVisit = [] #큐
         
     def linkComputers(self):
         for _ in range(self.pairs):
             source, target = map(int, input().split())
             self.computerGraph[source].append(target)
+            self.computerGraph[target].append(source)
             
     def checkInfected(self):
+        if self.computers == 0 or self.pairs == 0:
+            return 0
+        
+        self.needVisit.append(1)
         while len(self.needVisit) > 0:
-            
+            visitedComputer = self.needVisit.pop()
+            if not self.infectedComputers[visitedComputer] == 1:
+                self.infectedComputers[visitedComputer] = 1
+                self.needVisit.extend(self.computerGraph[visitedComputer])
+        return sum(self.infectedComputers) - 1
+        
+
+computers = int(input())
+linkedPairs = int(input())
+wormVirus = WormVirus(computers, linkedPairs)
+infectedComputersWithoutFirst = wormVirus.checkInfected()
+print(infectedComputersWithoutFirst)
