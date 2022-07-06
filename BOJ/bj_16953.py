12cef857217debee83d95eaf5009a3b2492dd9e3
hojooneum <bldolphin96@gmail.com>
2022-07-07 00:30:43 +0900

    [BOJ] bj_26953 DP solution is done

--- BOJ/bj_16953.py
+++ BOJ/bj_16953.py
@@ -1,30 +1,21 @@
-from collections import defaultdict
+from sys import maxsize
 A, B = map(int, input().split())
 
 def minOperations(A, B) -> int:
     if A == B:
         return 0
-    doubles = defaultdict(int)
-    doubles[A] = 0
-    padds = defaultdict(int)
-    padds[A] = 0
-    for i in range(A*2, B+1):
-        if i/2 in padds:
-            doubles[i] = padds[i//2] + 1
-        elif i/2 in doubles:
-            doubles[i] = doubles[i//2] + 1
-        elif (i-1)/10 in padds:
-            padds[i] = padds[(i-1)//10] + 1
-        elif (i-1)/10 in doubles:
-            padds[i] = doubles[(i-1)//10] + 1
+    ops = [None] * maxsize
+    ops[A] = 0
+    ops[A*2] = 1
+    ops[A*10 + 1] = 1
+    for i in range(A*2 + 1, B+1):
+        if (i & 1) == 0:
+            ops[i] = ops[i/2] + 1 if ops[i/2] != None else None
+        elif i % 10 == 1:
+            ops[i] = ops[(i-1)/10] + 1 if ops[(i-1)/10] != None else None
         else:
             continue
     
-    if B in doubles:
-        return doubles[B] + 1
-    elif B in padds:
-        return padds[B] + 1
-    else:
-        return -1
+    return ops[B] + 1 if ops[B] != None else -1
     
 print(minOperations(A,B))
