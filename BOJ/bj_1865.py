e604c7bfb45e0260a0cd541bbfb016183d94e5a2
hojooneum <bldolphin96@gmail.com>
2022-07-10 23:02:22 +0900

    [BOJ] Add bj_1865

---
+++ BOJ/bj_1865.py
@@ -0,0 +1,7 @@
+TCs = int(input())
+for tc in range(TCs):
+    N,M,W = map(int, input().split())
+    for i in range(M):
+        S,E,T = map(int, input().split())
+    for i in range(W):
+        S,E,T = map(int, input().split())
