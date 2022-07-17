308a34761a4b1106e2461c3bb0ac97547a5463c2
hojooneum <bldolphin96@gmail.com>
2022-07-17 12:33:01 +0900

    [BOJ]{SOLVED} bj_15686_v2

--- BOJ/bj_15686_v2.py
+++ BOJ/bj_15686_v2.py
@@ -8,16 +8,29 @@ def calculateCostsFromItems(costsForItems: list, houses: list, items: list) -> N
         cost = [getItemCosts(house, item) for house in houses]
         costsForItems.append(cost)
 
-def updateSavedItems(savedItems: list, newItem: int, constraint: int, minCostsFromHouse: list, houses: list, costsForItems: list) -> int:
-    pass
+def calculateMinCost(savedItems, costsForItems, houses) -> int:
+    CDC = 0
+    for house in range(len(houses)):
+        minCostFromHouse = maxsize
+        for item in range(len(savedItems)):
+            if costsForItems[item][house] < minCostFromHouse and savedItems[item] == 1:
+                minCostFromHouse = costsForItems[item][house]
+        CDC += minCostFromHouse
+    return CDC 
 
-def getMinCost(minCostsFromHouse, costsForItems, items, M, houses) -> int:
-    CCD = maxsize
-    savedItems = [0] * len(items)
-    for i in range(len(items)):
-        newCCD = updateSavedItems(savedItems, i, M, minCostsFromHouse, houses, costsForItems)
-        CCD = newCCD
-    return CCD
+def getMinCost(minCostsFromHouse, costsForItems, items, M, houses, savedItems, item):
+    if sum(savedItems) == M:
+        minCost = calculateMinCost(savedItems, costsForItems, houses)
+        if minCost < minCostsFromHouse[0]:
+            minCostsFromHouse[0] = minCost
+        return
+    else:
+        for i in range(item, len(items)):
+            if savedItems[i] != 1:
+                savedItems[i] = 1
+                getMinCost(minCostsFromHouse, costsForItems, items, M, houses, savedItems, i+1)
+                savedItems[i] = 0
+    return
 
 def main():
     N, M = map(int, input().split())
@@ -32,10 +45,10 @@ def main():
                 items.append((r,c))
 
     costsForItems = []
-
-    minCostsFromHouse = [maxsize] * len(houses)
+    savedItems = [0] * len(items)
+    minCostsFromHouse = [maxsize]
     calculateCostsFromItems(costsForItems, houses, items)
-    CCD = getMinCost(minCostsFromHouse, costsForItems, items, N, houses)
-    print(CCD)
+    getMinCost(minCostsFromHouse, costsForItems, items, M, houses, savedItems, 0)
+    print(minCostsFromHouse[0])
     
 main()
