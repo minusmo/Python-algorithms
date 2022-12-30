53ac44d8c5878880e1d56e089c9c6fe0c68d2f93
hojooneum <bldolphin96@gmail.com>
2022-12-30 23:58:02 +0900

    solving bj 2346

--- BOJ/bj_2346.py
+++ BOJ/bj_2346.py
@@ -3,23 +3,37 @@ input = sys.stdin.readline
 
 # 1 <= N <= 1000
 N = int(input())
+
+# (original idx, move)
 items = list(map(int, input().split()))
 
 pop_seq = []
 
 pop_idx = 0
-while len(items) != 0:
-    pop_seq.append(pop_idx)
-    next_move = items.pop(pop_idx)
-    if pop_idx + next_move > len(items):
-        next_pop = (pop_idx + next_move) % len(items) 
-    elif pop_idx + next_move <= len(items):
-        # -1
-        pass
-    elif pop_idx + next_move >= 0:
-        # just pop
-        pass
-    elif pop_idx + next_move < 0:
-        # +1
-        pass
-    
+while len(pop_seq) != N:
+    pop_seq.append(pop_idx+1)
+    if len(pop_seq) == N:
+        break
+    items[pop_idx] = 0
+    move = items[pop_idx]
+    right = True if move > 0 else False
+    steps = abs(move)
+    if right:
+        while steps > 0:
+            pop_idx += 1
+            pop_idx %= N
+            if items[pop_idx] == 0:
+                continue
+            else:
+                steps -= 1
+    else:
+        while steps > 0:
+            pop_idx -= 1
+            if pop_idx < 0:
+                pop_idx += N
+            if items[pop_idx] == 0:
+                continue
+            else:
+                steps -= 1
+        
+print(*pop_seq)
