74e653ef57a6255463ad6f82600c8a7ba6321c06
hojooneum <bldolphin96@gmail.com>
2022-12-31 00:05:11 +0900

    solve bj 2346

--- BOJ/bj_2346.py
+++ BOJ/bj_2346.py
@@ -10,30 +10,38 @@ items = list(map(int, input().split()))
 pop_seq = []
 
 pop_idx = 0
+def next_pop_idx_right(N, items, pop_idx, steps) -> int:
+    while steps > 0:
+        pop_idx += 1
+        pop_idx %= N
+        if items[pop_idx] == 0:
+            continue
+        else:
+            steps -= 1
+    return pop_idx
+
+def next_pop_idx_left(N, items, pop_idx, steps):
+    while steps > 0:
+        pop_idx -= 1
+        if pop_idx < 0:
+            pop_idx += N
+        if items[pop_idx] == 0:
+            continue
+        else:
+            steps -= 1
+    return pop_idx
+
 while len(pop_seq) != N:
     pop_seq.append(pop_idx+1)
     if len(pop_seq) == N:
         break
-    items[pop_idx] = 0
     move = items[pop_idx]
+    items[pop_idx] = 0
     right = True if move > 0 else False
     steps = abs(move)
     if right:
-        while steps > 0:
-            pop_idx += 1
-            pop_idx %= N
-            if items[pop_idx] == 0:
-                continue
-            else:
-                steps -= 1
+        pop_idx = next_pop_idx_right(N, items, pop_idx, steps)
     else:
-        while steps > 0:
-            pop_idx -= 1
-            if pop_idx < 0:
-                pop_idx += N
-            if items[pop_idx] == 0:
-                continue
-            else:
-                steps -= 1
+        pop_idx = next_pop_idx_left(N, items, pop_idx, steps)
         
 print(*pop_seq)
