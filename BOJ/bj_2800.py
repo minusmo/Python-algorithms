79bc7a8aa35b5934401a2c840ee1eab59d326e30
hojooneum <bldolphin96@gmail.com>
2023-01-04 23:51:35 +0900

    solving bj 2800

---
+++ BOJ/bj_2800.py
@@ -0,0 +1,23 @@
+import sys
+from collections import deque
+from itertools import combinations
+input = sys.stdin.readline
+
+expression = input().rstrip('\n')
+
+pairs = {}
+stack = deque()
+for item in enumerate(expression):
+    if item[1] == '(':
+        pairs[item[0]] = -1
+        stack.append(item)
+    elif item[1] == ')' and stack[-1][1] == '(':
+        pairs[stack[-1][0]] = item[0]
+
+for i in range(1,len(pairs)+1):
+    for combination in combinations(pairs.keys(), i):
+        exp = list(expression)
+        for position in combination:
+            exp.pop(position)
+            exp.pop(pairs[position])
+        print(''.join(exp))
