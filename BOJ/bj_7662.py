1ded69bbba9b5b70567f16eabc44bd3ddda11521
hojooneum <bldolphin96@gmail.com>
2023-01-15 14:04:57 +0900

    solved bj 7662

--- BOJ/bj_7662.py
+++ BOJ/bj_7662.py
@@ -1,53 +1,74 @@
 import sys
 from heapq import heappop, heappush
+from collections import defaultdict
 input = sys.stdin.readline
 
-class DBEQ:
-  def __init__(self) -> None:
-    self.is_removed = False
-  
-  def remove(self, heap: list, idx, x):
-    if self.is_removed or idx >= len(heap):
-      return
-    if heap[idx] == x:
-      left = heap[0:idx]
-      right = heap[idx+1:]
-      heap.clear()
-      heap.extend(left)
-      heap.extend(right)
-      self.is_removed = True
-    else:
-      self.remove(heap,idx*2+1,x)
-      self.remove(heap,idx*2+2,x)
-      
-results = []
+# unique container
+hash = defaultdict(int)
 minheap = []
 maxheap = []
+
 T = int(input())
 
 for _ in range(T):
   k = int(input())
   minheap.clear()
   maxheap.clear()
+  hash.clear()
   for _ in range(k):
     oper, n = input().strip().split()
     if oper == 'I':
-      heappush(minheap, int(n))
-      heappush(maxheap, -1*int(n))
+      n = int(n)
+      hash[n] += 1
+      heappush(minheap, n)
+      heappush(maxheap, -n)
     else:
-      if len(minheap) == 0:
-        continue
-      elif n == '1':
-        max_item = -1*heappop(maxheap)
-        DBEQ().remove(minheap,0,max_item)
+      if n == '1':
+        if len(maxheap) == 0:
+          continue
+        if -maxheap[0] in hash:
+          max_item = -heappop(maxheap)
+          hash[max_item] -= 1
+          if hash[max_item] == 0:
+            del hash[max_item]
+        else:
+          while not -maxheap[0] in hash:
+            heappop(maxheap)
+            if len(maxheap) == 0:
+              break
+          if len(maxheap) != 0:
+            max_item = -heappop(maxheap)
+            hash[max_item] -= 1
+            if hash[max_item] == 0:
+              del hash[max_item]
+        
       elif n == '-1':
-        min_item = heappop(minheap)
-        DBEQ().remove(maxheap,0,-1*min_item)
-
-  if len(minheap) == 0:
-    results.append('EMPTY')
+        if len(minheap) == 0:
+          continue
+        if minheap[0] in hash:
+          min_item = heappop(minheap)
+          hash[min_item] -= 1
+          if hash[min_item] == 0:
+            del hash[min_item]
+        else:
+          while not minheap[0] in hash:
+            heappop(minheap)
+            if len(minheap) == 0:
+              break
+          if len(minheap) != 0:
+            min_item = heappop(minheap)
+            hash[min_item] -= 1
+            if hash[min_item] == 0:
+              del hash[min_item]
+          
+  if len(hash) == 0:
+    print('EMPTY')
   else:
-    results.append('{0} {1}'.format(-1*maxheap[0], minheap[0]))
-
-for result in results:
-  print(result)
+    hash_vals = list(hash.keys())
+    max_val, min_val = hash_vals[0], hash_vals[0]
+    for item in hash_vals:
+      if item > max_val:
+        max_val = item
+      if item < min_val:
+        min_val = item
+    print('{0} {1}'.format(max_val, min_val))
