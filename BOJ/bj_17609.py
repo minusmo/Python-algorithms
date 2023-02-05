b1792d17497c99ccba340a7be970ab40f4736ab2
hojooneum <bldolphin96@gmail.com>
2023-02-05 23:58:30 +0900

    still solving bj 17609

--- BOJ/bj_17609.py
+++ BOJ/bj_17609.py
@@ -1,34 +1,54 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
 T = int(input())
 for _ in range(T):
-  string = input().strip()
+  raw_string = input().strip()
+  string = deque(list(raw_string))
   result = 2
+  front, back = 0, len(raw_string)-1
   if len(string)%2 == 0:
-    left_half = string[:len(string)//2]
-    right_half = ''.join(list(reversed(string[len(string)//2:])))
-    if left_half == right_half:
+    while front < back:
+      if string[front] == string[back]:
+        front += 1
+        back -= 1
+      else:
+        break
+    if front > back:
       result = 0
   else:
-    left_half = string[:len(string)//2]
-    right_half = ''.join(list(reversed(string[len(string)//2+1:])))
-    if left_half == right_half:
+    while front != back:
+      if string[front] == string[back]:
+        front += 1
+        back -= 1
+      else:
+        break
+    if front == back:
       result = 0
   
   if result != 0:
-    for i in range(len(string)):
+    for i in range(len(raw_string)):
       if result == 1:
         break
-      string_without_char = string[:i] + string[i+1:]
+      string_without_char = raw_string[:i] + raw_string[i+1:]
+      front, back = 0, len(string_without_char)-1
       if len(string_without_char)%2 == 0:
-        left_half = string_without_char[:len(string_without_char)//2]
-        right_half = ''.join(list(reversed(string_without_char[len(string_without_char)//2:])))
-        if left_half == right_half:
+        while front < back:
+          if string_without_char[front] == string_without_char[back]:
+            front += 1
+            back -= 1
+          else:
+            break
+        if front > back:
           result = 1
       else:
-        left_half = string_without_char[:len(string_without_char)//2]
-        right_half = ''.join(list(reversed(string_without_char[len(string_without_char)//2+1:])))
-        if left_half == right_half:
+        while front != back:
+          if string_without_char[front] == string_without_char[back]:
+            front += 1
+            back -= 1
+          else:
+            break
+        if front == back:
           result = 1
   
   print(result)
